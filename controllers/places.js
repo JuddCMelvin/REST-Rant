b4a5faa4be0804fd0afb866a92d358cafbe8ead4
Judd Chalmers Melvin <juddmelvinhtmldev@gmail.com>
2024-04-27 09:43:20 -0400

    Form data is getting through git push origin main

--- controllers/places.js
+++ controllers/places.js
@@ -5,9 +5,14 @@ router.get('/new', (req, res) => {
     res.render('places/new')
 })
 
-router.get('/:id', (req, res) => {
-    res.render('places/index', {places})
+router.post('/', (req, res) => {
+    console.log(req.body)
+    res.send('POST /places')
 })
+
+// router.get('/:id', (req, res) => {
+//     res.render('places/index', {places})
+// })
 
 module.exports = router
 
