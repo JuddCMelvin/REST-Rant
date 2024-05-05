00eb6fe07c5f3f12bb66662e14afbfec9a6f4ffc
Judd Chalmers Melvin <juddmelvinhtmldev@gmail.com>
2024-05-04 20:47:50 -0400

    complete part 9

--- views/places/show.jsx
+++ views/places/show.jsx
@@ -1,22 +1,50 @@
 const React = require('react')
 const Def = require('../default')
 
-function show (data) {
+function show ({place}) {
     return (
         <Def>
             <main>
-                <h1>{ data.place.name }</h1>
-                <a href={`/places/${data.id}/edit`} className="btn btn-warning"> 
-                    Edit
-                </a>     
-                <form method="POST" action={`/places/${data.id}?_method=DELETE`}> 
-                    <button type="submit" className="btn btn-danger">
-                        Delete
-                    </button>
-                </form> 
+            <div className="row">
+                <div className="col-sm-6">
+                <img src={place.pic} alt={place.name} />
+                <h3>
+                    Located in {place.city}, {place.state}
+                </h3>
+                </div>
+                <div className="col-sm-6">
+                <h2>Rating</h2>
+                <h3>
+                    Not Rated
+                </h3>
+                <h2>
+                    Description
+                </h2>
+                <h3>
+                    {place.showEstablished()}
+                </h3>
+                <h4>
+                    Serving {place.cuisines}
+                </h4>
+                <a href={`/places/${place.id}/edit`} className="btn btn-warning">Edit</a>
+                <form method="POST" action={`/places/${place.id}?_method=DELETE`}> 
+                    <button type="submit" className="btn btn-danger">Delete</button>
+                </form>
+                </div>
+                <div className="col-sm-12">
+                    <h2>
+                    Comments
+                    </h2>
+                    <h3>
+                    No Comments
+                    </h3>
+                </div>
+            </div>
             </main>
         </Def>
     )
 }
+
+module.exports = show
 
 module.exports = show
