3691b1424acc4865c0b84cb5ee7b3a64ac171878
Judd Chalmers Melvin <juddmelvinhtmldev@gmail.com>
2024-06-01 16:36:25 -0400

    can't figure out issue with edit

--- models/index.js
+++ models/index.js
@@ -2,12 +2,12 @@ require('dotenv').config()
 const mongoose = require('mongoose')
 
 mongoose.connect(process.env.MONGO_URI, {
-    useNewUrlParser: true, 
-    useUnifiedTopology: true
+  useNewUrlParser: true, 
+  useUnifiedTopology: true
 })
 
-module.exports.Place = require('./places.js')
-module.exports.Comment = require('./comment.js')
+module.exports.Place = require('./places')
+module.exports.Comment = require('./comment')
 
 
 
